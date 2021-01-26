3d6f5e96e20c0cd09c6124464724682e52e868b5
Spencer Will <spencer.a.will@gmail.com>
2021-01-25 21:35:18 -0500

    Fixed timer class. Still need to fix it in device.ino

--- device/Timer.cpp
+++ device/Timer.cpp
@@ -1,30 +1,90 @@
 #include "Arduino.h"
 #include "Timer.h"
 
-int _counter;
-int Threshold;
-bool Enabled;
+unsigned int _threshold;
+unsigned int _counter;
+bool _paused;
+void (*_timerFinishedCallback)(Timer);
 
-Timer::Timer(int threshold) {
-	Threshold = threshold;
-	Enabled = true;
-	_counter = 0;
-};
+Timer::Timer(unsigned int seconds)
+{
+  _threshold = seconds;
+  Reset();
+}
 
-int Timer::GetProgress() {
-	return _counter;
-};
+Timer::Timer(unsigned int seconds, void (*timerFinishedCallback)(Timer))
+{
+  _threshold = seconds;
+  SetTimerFinishedCallback(timerFinishedCallback);
+  Reset();
+}
 
-bool Timer::CounterAboveThreshold() {
-	return _counter >= Threshold;
-};
+bool Timer::IsPaused()
+{
+  return _paused;
+}
 
-void Timer::Increment() {
-	if (Enabled) {
-		_counter++;
-	}
-};
+void Timer::Pause()
+{
+  if (!IsPaused())
+  {
+    _paused = true;
+  }
+}
 
-void Timer::Reset() {
-	_counter = 0;
-};
+void Timer::Resume()
+{
+  if (IsPaused())
+  {
+    _paused = false;
+  }
+}
+
+void Timer::Reset()
+{
+  _counter = 0;
+  Pause();
+}
+
+void Timer::Restart()
+{
+  Reset();
+  Resume();
+}
+
+bool Timer::TimerFinished()
+{
+  return _counter >= _threshold;
+}
+
+unsigned int Timer::GetElapsed()
+{
+  return _threshold;
+}
+
+void Timer::Increment(unsigned int seconds)
+{
+  if (!(TimerFinished() || !IsPaused()))
+  {
+    _counter += seconds;
+    if (TimerFinished())
+    {
+      _timerFinishedCallback(this);
+    }
+  }
+
+}
+
+void Timer::Increment()
+{
+  Increment(1);
+}
+
+
+void Timer::SetTimerFinishedCallback(void (*timerFinishedCallback)(Timer))
+{
+  if (timerFinishedCallback)
+  {
+    _timerFinishedCallback = timerFinishedCallback;
+  }
+}
--- device/Timer.h
+++ device/Timer.h
@@ -4,21 +4,25 @@
 #include "Arduino.h"
 
 class Timer {
-	private:
-		int _counter;
-	public:
-		int Threshold;
-		bool Enabled;
-
-		int GetProgress();
-
-		bool CounterAboveThreshold();
-
-		void Increment();
-
-		void Reset();
-
-		Timer(int threshold);
+  private:
+    unsigned int _threshold;
+    unsigned int _counter;
+    bool _paused;
+    void (*_timerFinishedCallback)(Timer);
+    void emptyCallback();
+  public:
+    bool IsPaused();
+    unsigned int GetElapsed();
+    Timer(unsigned int seconds);
+    Timer(unsigned int seconds, void (*timerFinishedCallback)(Timer));
+    void Pause();
+    void Resume();
+    void Reset();
+    void Restart();
+    void SetTimerFinishedCallback(void (*timerFinishedCallback)(Timer));
+    bool TimerFinished();
+    void Increment(unsigned int seconds);
+    void Increment();
 };
 
-#endif // !Timer_h
+#endif
